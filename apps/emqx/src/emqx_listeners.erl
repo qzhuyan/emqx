e89c73ff8296948d95556366dc9c8fbb8aa2828e
William Yang <mscame@gmail.com>
2021-08-25 17:04:22 +0200

    feat(quic): at least 8 x number of cores acceptors

--- apps/emqx/src/emqx_listeners.erl
+++ apps/emqx/src/emqx_listeners.erl
@@ -148,7 +148,7 @@ do_start_listener(ZoneName, ListenerName, #{type := quic, bind := ListenOn} = Op
             ListenOpts = [ {cert, maps:get(certfile, Opts)}
                          , {key, maps:get(keyfile, Opts)}
                          , {alpn, ["mqtt"]}
-                         , {conn_acceptors, maps:get(acceptors, Opts, DefAcceptors)}
+                         , {conn_acceptors, lists:max([DefAcceptors, maps:get(acceptors, Opts, 0)])}
                          , {idle_timeout_ms, lists:max([
                                                 emqx_config:get_zone_conf(ZoneName, [mqtt, idle_timeout]) * 3
                                               , timer:seconds(maps:get(idle_timeout, Opts))]
