93ced1807474ec753971ff6c71840041956ac2a4
William Yang <mscame@gmail.com>
2025-09-02 18:58:45 +0200

    chore: bump quicer 0.1.12
    For smaller packaging, no functional changes

--- mix.exs
+++ mix.exs
@@ -1234,7 +1234,7 @@ defmodule EMQXUmbrella.MixProject do
     if enable_quicer?(),
       # in conflict with emqx and emqtt
       do: [
-        {:quicer, github: "emqx/quic", tag: "0.1.11", override: true}
+        {:quicer, github: "emqx/quic", tag: "0.1.12", override: true}
       ],
       else: []
   end
